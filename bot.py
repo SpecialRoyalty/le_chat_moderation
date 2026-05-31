f950d5e822d34fa53d19958b34122a1d2ebffede
SpecialRoyalty <nataliatupalskaparis@gmail.com>
2026-05-31 18:50:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1767,7 +1767,7 @@ async def save_user_message_if_session(update: Update):
 # =========================
 
 MSG_PARTICIPATION_REQUIRED = "⚠️ Merci de participer avant d’envoyer un message.\nEnvoyez au moins 1 photo ou 1 vidéo jamais publiée."
-MSG_REPOST = "♻️ Ce média a déjà été publié."
+MSG_REPOST = "♻️ Ton média a déjà été publié par quelqu'un d'autre !"
 MSG_LINK_FORBIDDEN = "🔗 Les liens ne sont pas autorisés."
 MSG_FORWARD_FORBIDDEN = "🚫 Les transferts ne sont pas autorisés."
 MSG_GENERIC_FORBIDDEN = "🚫 Message non autorisé."

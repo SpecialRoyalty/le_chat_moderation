54a5b720f36b21bfaa2ac302d4ac6b40a5df4a01
SpecialRoyalty <nataliatupalskaparis@gmail.com>
2026-06-21 20:32:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -347,7 +347,7 @@ async def close_session_admin(ctx=None):
         except Exception as e:
             print(f'NONPARTICIPANT CLEANUP CLOSE ERROR: {e}',flush=True)
         try:
-            await send_or_edit_session_status(ctx,sid,'🔴 Session fermée\n\nMerci à tous les participants d'attendre la prochaine ouverture.')
+            await send_or_edit_session_status(ctx,sid,'🔴 Session fermée\n\nMerci à tous les participants d attendre la prochaine ouverture.')
         except Exception as e:
             print(f'SESSION PUBLIC CLOSE STATUS ERROR #{sid}: {e}',flush=True)
         try:
